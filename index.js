8a650408b748e14af0d01a763b9e9881e5d9a6a4
Saurav Suman <sauravsuman0199@gmail.com>
2024-03-02 17:23:12 +0530

    modified

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ accordians.forEach(accordian=>{
      const answer = accordian.querySelector(".answer");
 
      accordian.addEventListener('click',()=>{
-        icon.classList.toggle('active');
-        answer.classList.toggle('active')
+        // icon.classList.toggle('active');
+        // answer.classList.toggle('active')
+        if(icon.classList.contains('active')){
+            icon.classList.remove('active')
+            answer.style.maxHeight = null;
+        }else{
+            icon.classList.add('active');
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+        }
      })
 
 })
